e71648861f42ca57580a432299c0ac6b1b07f7fb
Priti Bedre <pritinbedre22@gmail.com>
2026-03-15 03:27:04 +0530

    fix Railway connection for Render

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,23 +1,23 @@
 const mysql = require("mysql2");
 
-const isProd = process.env.NODE_ENV === "production";
+// Pool variable
+let pool;
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-
-  ssl: isProd
-    ? {
-        rejectUnauthorized: false, // OK for Aiven + Render
-      }
-    : undefined,
-});
+if (process.env.DATABASE_URL) {
+  // Production / Render → use Railway public URL
+  pool = mysql.createPool(process.env.DATABASE_URL);
+} else {
+  // Local development → use individual variables
+  pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  });
+}
 
 module.exports = pool;
